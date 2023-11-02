f3748e9b17de1c42d5e42c3396fc9b5083e9ed2c
huxw <huxw@mail.jj.cn>
2023-11-03 00:32:41 +0800

    初始化项目

---
+++ src/config/index.ts
@@ -0,0 +1,28 @@
+/**
+ * yai Project的信息
+ */
+interface YPIProject {
+    /**
+     * 名称
+     */
+    name: string;
+    /**
+     * id
+     */
+    id: string;
+}
+
+
+/**
+ * 转换配置
+ */
+interface TransFormConfigItem {
+    /**
+     * 白名单
+     */
+    whiteList: [string | RegExp][];
+    /**
+     * 类别 分类或者api
+     */
+    type: "cate" | "api";
+}
---
+++ src/schema/index.ts
@@ -0,0 +1,6 @@
+import { JSONSchema4 } from "json-schema";
+import { compile, Options } from "json-schema-to-typescript";
+
+export function jsonSchemeToTypeScript(schema: JSONSchema4, name: string, options: Partial<Options> | undefined) {
+    return compile(schema, name, options);
+}
