557f15b4fcab3593e43ddb38506597a1ff5408ff
huxw <huxw@mail.jj.cn>
2023-11-13 21:02:05 +0800

    修改res.body

--- src/code/templates/type.res.body.ts
+++ src/code/templates/type.res.body.ts
@@ -15,21 +15,26 @@ export default async function generateResBodyType(eApi: EAPIItem) {
     }
     const typeName = type!.resBodyTypeName!;
     // 响应是JSON Schema
-    if (api.res_body_type === "json" && api.res_body_is_json_schema) {
-        const schema: JSONSchema4 = JSON.parse(api.res_body || "{}");
-        if (!schema.description) {
-            schema.description = `${api.title}响应结果\r\npath: ${api.path}\r\ndoc url: ${type?.docUrl}`;
+    if (api.res_body_type === "json") {
+        if (api.res_body_is_json_schema) {
+            const schema: JSONSchema4 = JSON.parse(api.res_body || "{}");
+            if (!schema.description) {
+                schema.description = `${api.title}响应结果\r\npath: ${api.path}\r\ndoc url: ${type?.docUrl}`;
+            }
+            schema.title = typeName;
+            code = await jsonSchemeToTypeScript(schema, typeName, {
+                bannerComment: "",
+            });
+            return {
+                schema,
+                code,
+                typeName,
+                apiInfo: api,
+            };
         }
-        schema.title = typeName;
-        code = await jsonSchemeToTypeScript(schema, typeName, {
-            bannerComment: "",
-        });
-        return {
-            schema,
-            code,
-            typeName,
-            apiInfo: api,
-        };
     }
-    return null;
+    return {
+        code: `
+    export interface ${typeName} ${api.res_body}
+    `};
 }
