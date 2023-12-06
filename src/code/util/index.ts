c5c0417a651eed23c6e824c114ef1acfc2efb44d
huxw <huxw@mail.jj.cn>
2023-12-06 09:48:44 +0800

    合并

--- src/code/util/index.ts
+++ src/code/util/index.ts
@@ -9,7 +9,7 @@ export function getFullApiDocUrl(params: {
     return `${params.server}/project/${params.projectId}/interface/api/${params.apiId}`;
 }
 
-export function replaceRequestPrefix(value: string ,paramObject: Record<string, any>) {
+export function replaceRequestPrefix(value: string, paramObject: Record<string, any>) {
     const rValue = value.replace(/\$\{/gim, "\\${");
     return createOneParamFunction("return \\`" + rValue + "\\`", ['R', 'method'])(
         paramObject
@@ -23,5 +23,16 @@ export function genRequestPrefix(eApi: EAPIItem) {
         method: eApi.api.method.toLocaleLowerCase(),
     })
     return result
- }
+}
 
+export function fixTypesFilePath(relativePath: string) {
+    let fp = relativePath;
+    if (fp.endsWith(".ts")) {
+        fp = fp.slice(0, -3);
+    }
+    fp = fp.replace(/\\/img, "/");
+    if (!fp.startsWith(".") || !fp.startsWith("/")) {
+        return `./${fp}`
+    }
+    return fp;
+}
